918824cc0f7b475ba177efc8898e631f739fa6b2
Kjell Post <kjell@irstafoto.se>
2026-01-06 04:25:58 +0100

    added inc-button

---
+++ inc-button/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+let val = 0;
+const incHandler = () => { val++; }
+</script>
+
+<h1>This is project <span class="mono">inc-button</span></h1>
+<p>Enter a value or start clicking on the button</p>
+
+<button on:click={incHandler}>Increment</button>
+<!-- You can also inline the function: -->
+<!-- <button on:click={() => { val++; }}>Increment</button> -->
+
+<!-- with this construction, the user can not enter a new value -->
+<!-- <input type="number" value={val} /> -->
+<!-- with this construction, the user can enter a new value -->
+<input type="number" bind:value={val} />
+
+<style>
+.mono { font-family: Courier; }
+</style>
